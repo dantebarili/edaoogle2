cc9134fc6e60369639c3747b38215cebed4d7b3c
dantebarili <dantebarili1@gmail.com>
2024-11-06 17:32:40 -0300

    DD

--- HttpRequestHandler.cpp
+++ HttpRequestHandler.cpp
@@ -11,15 +11,11 @@
 #include <fstream>
 #include <iostream>
 #include <sqlite3.h>
+#include <vector>
 
 #include "HttpRequestHandler.h"
 
 using namespace std;
-
-struct SearchResult {
-    string pageName;
-    int totalFrequency;
-};
 
 HttpRequestHandler::HttpRequestHandler(string homePath)
 {
@@ -62,31 +58,9 @@ bool HttpRequestHandler::serve(string url, vector<char> &response)
     return true;
 }
 
-bool insertDataIntoFTS(sqlite3* db) {
-    const char* insertDataQuery = R"(
-        INSERT INTO keyword_index_fts (keyword, URL, frequency)
-        SELECT keyword, URL, frequency FROM keyword_index;
-    )";
-
-    sqlite3_stmt* stmt;
-    if (sqlite3_prepare_v2(db, insertDataQuery, -1, &stmt, nullptr) == SQLITE_OK) {
-        if (sqlite3_step(stmt) != SQLITE_DONE) {
-            cerr << "Error al insertar los datos en la tabla FTS: " << sqlite3_errmsg(db) << endl;
-            sqlite3_finalize(stmt);
-            return false;
-        }
-        sqlite3_finalize(stmt);
-        cout << "Datos insertados en la tabla FTS correctamente." << endl;
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-
 bool createFTSTable(sqlite3* db) {
     const char* createTableQuery = R"(
-        CREATE VIRTUAL TABLE keyword_index_fts USING fts5(keyword, URL, frequency);
+        CREATE VIRTUAL TABLE IF NOT EXISTS keyword_index_fts USING fts5(keyword, URL, frequency);
     )";
 
     sqlite3_stmt* stmt;
@@ -101,11 +75,35 @@ bool createFTSTable(sqlite3* db) {
         return true;
     }
     else {
+        cerr << "Error al crear la tabla FTS: " << sqlite3_errmsg(db) << endl;
         return false;
     }
 }
 
-bool searchUsingFTS(sqlite3* db, const string& searchString, vector<SearchResult>& results) {
+bool insertDataIntoFTS(sqlite3* db) {
+    const char* insertDataQuery = R"(
+        INSERT INTO keyword_index_fts (keyword, URL, frequency)
+        SELECT keyword, url, frequency FROM keyword_index;
+    )";
+
+    sqlite3_stmt* stmt;
+    if (sqlite3_prepare_v2(db, insertDataQuery, -1, &stmt, nullptr) == SQLITE_OK) {
+        if (sqlite3_step(stmt) != SQLITE_DONE) {
+            cerr << "Error al insertar los datos en la tabla FTS: " << sqlite3_errmsg(db) << endl;
+            sqlite3_finalize(stmt);
+            return false;
+        }
+        sqlite3_finalize(stmt);
+        cout << "Datos insertados en la tabla FTS correctamente." << endl;
+        return true;
+    }
+    else {
+        cerr << "Error al preparar la consulta de insercion: " << sqlite3_errmsg(db) << endl;
+        return false;
+    }
+}
+
+bool searchUsingFTS(sqlite3* db, const string& searchString, vector<string>& results) {
     const char* query = R"(
         SELECT URL, SUM(frequency) AS total_frequency
         FROM keyword_index_fts
@@ -120,13 +118,9 @@ bool searchUsingFTS(sqlite3* db, const string& searchString, vector<SearchResult
 
         while (sqlite3_step(stmt) == SQLITE_ROW) {
             const char* pageName = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0));
-            int totalFrequency = sqlite3_column_int(stmt, 1);
 
             if (pageName) {
-                SearchResult result;
-                result.pageName = pageName;
-                result.totalFrequency = totalFrequency;
-                results.push_back(result);  // Agregar el resultado al vector
+                results.push_back(pageName); 
             }
         }
 
@@ -134,7 +128,7 @@ bool searchUsingFTS(sqlite3* db, const string& searchString, vector<SearchResult
         return true;
     }
     else {
-        cerr << "Error al preparar la consulta de búsqueda: " << sqlite3_errmsg(db) << endl;
+        cerr << "Error al buscar con FTS: " << sqlite3_errmsg(db) << endl;
         return false;
     }
 }
@@ -153,54 +147,26 @@ bool HttpRequestHandler::handleRequest(string url,
 
         // Conectar a la base de datos SQLite
         sqlite3* db;
-        if (sqlite3_open("database.db", &db) != SQLITE_OK)
+        if (sqlite3_open("C:/Users/dante/OneDrive/Documentos/git/edaoogle2/search_index.db", &db) != SQLITE_OK)
         {
             cerr << "Error al abrir la base de datos" << endl;
             return false;
         }
 
         // Vector para almacenar los nombres de las páginas en el orden deseado
-        vector<SearchResult> results;
+        vector<string> results;
 
         // Crear tabla FTS e insterarle los datos
-        if (!createFTSTable(db))
-            cerr << "Error al crear la tabla FTS: " << sqlite3_errmsg(db) << endl;
-
-        if(!insertDataIntoFTS(db))
-            cerr << "Error al preparar la consulta de inserción: " << sqlite3_errmsg(db) << endl;
-
-        if(!searchUsingFTS(db, searchString, results))
-            cerr << "Error al buscar con FTS: " << sqlite3_errmsg(db) << endl;
-
-        // Preparar la consulta para realizar la búsqueda con FTS5
-        sqlite3_stmt* stmt;
-        string query = R"(
-            SELECT url, SUM(frequency) AS total_frequency
-            FROM keyword_index
-            WHERE keyword = ?
-            GROUP BY url
-            ORDER BY total_frequency DESC
-        )";
-
-        if (sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, nullptr) == SQLITE_OK)
-        {
-            // Enlaza el término de búsqueda a la consulta
-            sqlite3_bind_text(stmt, 1, searchString.c_str(), -1, SQLITE_STATIC);
-
-            // Ejecutar la consulta y obtener los nombres de las páginas en orden de frecuencia
-            while (sqlite3_step(stmt) == SQLITE_ROW)
-            {
-                const char* pageName = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0));
-                if (pageName)
-                    results.push_back(pageName);
-            }
-            sqlite3_finalize(stmt);
+        if (!createFTSTable(db)) {      
+            return 1;
         }
-        else
-        {
-            cerr << "Error al preparar la consulta" << endl;
-            sqlite3_close(db);
-            return false;
+
+        if(!insertDataIntoFTS(db)){            
+            return 1;
+        }
+
+        if(!searchUsingFTS(db, searchString, results)){            
+            return 1;
         }
 
         sqlite3_close(db);
@@ -251,68 +217,3 @@ bool HttpRequestHandler::handleRequest(string url,
 
     return false;
 }
-
-
-/*
-bool HttpRequestHandler::handleRequest(string url,
-                                               HttpArguments arguments,
-                                               vector<char> &response)
-{
-    string searchPage = "/search";
-    if (url.substr(0, searchPage.size()) == searchPage)
-    {
-        string searchString;
-        if (arguments.find("q") != arguments.end())
-            searchString = arguments["q"];
-
-        // Header
-        string responseString = string("<!DOCTYPE html>\
-<html>\
-\
-<head>\
-    <meta charset=\"utf-8\" />\
-    <title>EDAoogle</title>\
-    <link rel=\"preload\" href=\"https://fonts.googleapis.com\" />\
-    <link rel=\"preload\" href=\"https://fonts.gstatic.com\" crossorigin />\
-    <link href=\"https://fonts.googleapis.com/css2?family=Inter:wght@400;800&display=swap\" rel=\"stylesheet\" />\
-    <link rel=\"preload\" href=\"../css/style.css\" />\
-    <link rel=\"stylesheet\" href=\"../css/style.css\" />\
-</head>\
-\
-<body>\
-    <article class=\"edaoogle\">\
-        <div class=\"title\"><a href=\"/\">EDAoogle</a></div>\
-        <div class=\"search\">\
-            <form action=\"/search\" method=\"get\">\
-                <input type=\"text\" name=\"q\" value=\"" +
-                                       searchString + "\" autofocus>\
-            </form>\
-        </div>\
-        ");
-
-        // YOUR JOB: fill in results
-        float searchTime = 0.1F;
-        vector<string> results;
-
-        // Print search results
-        responseString += "<div class=\"results\">" + to_string(results.size()) +
-                          " results (" + to_string(searchTime) + " seconds):</div>";
-        for (auto &result : results)
-            responseString += "<div class=\"result\"><a href=\"" +
-                              result + "\">" + result + "</a></div>";
-
-        // Trailer
-        responseString += "    </article>\
-</body>\
-</html>";
-
-        response.assign(responseString.begin(), responseString.end());
-
-        return true;
-    }
-    else
-        return serve(url, response);
-
-    return false;
-}
-*/
